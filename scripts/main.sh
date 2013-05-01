ef1759038d9caf202f9318c788daa7454197ecaf
badmotherfucker <ilya.pimenov@gmail.com>
2013-05-01 03:32:25 +0200

    Implemented help section in main.sh and check for input

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,25 +2,38 @@
 
 filter_pattern="http://scripts.adrcdn.com/000394/scripts/screenad_launch_9.4.0_scrambled.js"
 processed=processed_files
+usage="$(basename "$0") <directory> -- Screen6 log converting application
 
-if [ ! -e "$processed" ] ; then
-    touch "$processed"
+where:
+    <directory> to scan for compressed access log files"
+
+usage(){
+	echo "$usage"
+}
+
+if ( [[ $# -eq 0 ]] && usage )
+	then
+	exit 1
+fi
+
+if [ ! -e "$processed" ] ; 
+	then
+	touch "$processed"
 fi
 
 for i in $1/* 
 do
-    if test -f "$i" 
-    then
-       if grep -Fxq "$i" $processed
-       then
-           echo "Already processed $i"
-       else
-           echo "Processing $i"
-           filename=$(basename "$i")
-           processed_filename="${filename%.*.*}".processed.gz
-           time gunzip -c $i | grep $filter_pattern | ./Screen6GawkLogProcessor.sh | gzip -c > $processed_filename
-           echo $i >> $processed
-       fi
-    fi
+	if test -f "$i" 
+		then
+		if grep -Fxq "$i" $processed 
+			then
+			echo "Already processed $i"
+		else
+			echo "Processing $i"
+			filename=$(basename "$i")
+			processed_filename="${filename%.*.*}".processed.gz
+			time gunzip -c $i | grep $filter_pattern | java Screen6LogProcessor | gzip -c > $processed_filename
+			echo $i >> $processed
+		fi
+	fi
 done
-
